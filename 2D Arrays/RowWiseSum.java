b1c6b32311ddc3f35ed908e30cd7c4a4515e6d6f
Ashutosh Maurya <92643685+ashu1230@users.noreply.github.com>
2022-12-21 09:09:30 +0530

    Update RowWiseSum.java

--- 2D Arrays/RowWiseSum.java
+++ 2D Arrays/RowWiseSum.java
@@ -20,6 +20,7 @@ public class Solution {
     public static void main(String[] args) {
 	    
 		// TODO Auto-generated method stub
+	    
 		Scanner sc = new Scanner(System.in);
 		int r = sc.nextInt();
 		int c = sc.nextInt();
